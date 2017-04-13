7494877abf31554cc783ba2ba00d21d1e30c312d
TalZa <tal.zaidman1810@gmail.com>
2017-04-13 09:41:30 +0300

    adding css to form and thank you page

--- contact-form/app.js
+++ contact-form/app.js
@@ -22,8 +22,7 @@ app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __di
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,7 +34,8 @@ app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false})
 //define which pages to render
 app.use('/', index);
 
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
